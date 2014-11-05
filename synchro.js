57b69e32db6e8b402696e80218736a0801984964
Clément TALVARD <clement@lumapps.com>
2014-11-05 23:13:00 +0100

    correct firebase update

--- synchro.js
+++ synchro.js
@@ -3,7 +3,7 @@ function initSynchro(){
   myDataRef = new Firebase('https://resplendent-inferno-5296.firebaseio.com/');
 
   //get Data
-  myDataRef.on('child_added', function(snapshot) {
+  myDataRef.on('child_changed', function(snapshot) {
     var player = snapshot.val();
     for (var i = 0; i < players.length; i++) {
       if (players[i].name === player.name) {
@@ -16,11 +16,11 @@ function initSynchro(){
 
 //log new player on the game
 function login()  {
-  var player1;
+  var player;
 
   for (var i = 0; i < players.length; i++){
     if (players[i].name == getNomJoueur()) {
-      player1 = players[i];
+      player = players[i];
     }
   }
 
@@ -29,39 +29,45 @@ function login()  {
     console.log(gameGrid.length);
     if (event.keyCode === 40){
       console.log('up!');
-      if (player1.y < gameGrid.length-1) {
-        player1.y++;
+      if (player.y < gameGrid.length-1) {
+        player.y++;
       } else {
-        player1.y = 0;
+        player.y = 0;
       }
     }
     //down
     if (event.keyCode === 38){
       console.log('down!');
-      if (player1.y != 0) {
-        player1.y--;
+      if (player.y != 0) {
+        player.y--;
       } else {
-        player1.y = gameGrid.length-1;
+        player.y = gameGrid.length-1;
       }
     }
     //left
     if (event.keyCode === 37){
       console.log('left!');
-      if (player1.x != 0) {
-        player1.x--;
+      if (player.x != 0) {
+        player.x--;
       } else {
-        player1.x = gameGrid[0].length-1;
+        player.x = gameGrid[0].length-1;
       }
     }
     //right
     if (event.keyCode === 39){
       console.log('right!');
-      if (player1.x < gameGrid[0].length-1) {
-        player1.x++;
+      if (player.x < gameGrid[0].length-1) {
+        player.x++;
       } else {
-        player1.x = 0;
+        player.x = 0;
       }
     }
-    myDataRef.push(player1);
+
+    for (var i = 0; i < players.length; i++) {
+      if (players[i].name === player.name) {
+        players[i] = player;
+      }
+    }
+    myDataRef.set(players);
   }, false);
 }
